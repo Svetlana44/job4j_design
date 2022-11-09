a7fc50316ac59ee69e63169cb92361fc0d3a3694
SpS <shchaaas@mail.ru>
2022-11-09 21:20:04 +0300

    3. Удалить head в односвязном списке. [#51424]

--- src/main/java/ru/job4j/collection/ForwardLinked.java
+++ src/main/java/ru/job4j/collection/ForwardLinked.java
@@ -7,7 +7,7 @@ public class ForwardLinked<T> implements Iterable<T> {
     private Node<T> head;
 
     public void add(T value) {
-        Node<T> node = new Node<T>(value, null);
+        Node<T> node = new Node<>(value, null);
         if (head == null) {
             head = node;
             return;
