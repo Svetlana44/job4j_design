ccc20087290474c9943785b428642df339690634
SpS <shchaaas@mail.ru>
2022-12-10 15:55:54 +0300

    1. Создать модель User [#999]

---
+++ src/main/java/ru/job4j/collection/User.java
@@ -0,0 +1,15 @@
+package ru.job4j.collection;
+
+import java.util.Calendar;
+
+public class User {
+    private String name;
+    private int children;
+    private Calendar birthday;
+
+    User(String name, int children, Calendar birthday) {
+        this.name = name;
+        this.children = children;
+        this.birthday = birthday;
+    }
+}
