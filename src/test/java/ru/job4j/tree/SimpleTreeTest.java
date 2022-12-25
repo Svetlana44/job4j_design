bfe4c59f338566f5e3074599b2d007d638cebf05
SpS <shchaaas@mail.ru>
2022-12-25 22:00:39 +0300

    2. Добавить метод boolean isBinary() [#1712]

--- src/test/java/ru/job4j/tree/SimpleTreeTest.java
+++ src/test/java/ru/job4j/tree/SimpleTreeTest.java
@@ -3,6 +3,8 @@ package ru.job4j.tree;
 import org.junit.jupiter.api.Test;
 import ru.job4j.tree.Tree.Node;
 
+import java.util.function.Predicate;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class SimpleTreeTest {
@@ -49,5 +51,41 @@ public class SimpleTreeTest {
         assertThat(tree.findBy(3)).isPresent();
         assertThat(tree.findBy(3).get()).isEqualTo(new Node<>(3));
         assertThat(tree.findBy(2)).isNotPresent();
+    }
+
+    @Test
+    void whenFindByPredicate() {
+        SimpleTree<Integer> tree = new SimpleTree<>(1);
+        tree.add(1, 2);
+        tree.add(1, 3);
+        tree.add(1, 4);
+        Predicate<Node<Integer>> predicate = node -> node.children.size() > 2;
+        Node<Integer> node = new Node<>(1);
+
+        /* if node.children will not final, then may be:
+        node.children = Arrays.asList(new Node<>(2), new Node<>(3), new Node<>(4));
+         */
+        assertThat(tree.findByPredicate(predicate)).isPresent();
+    }
+
+    @Test
+    void whenIsBinary() {
+        SimpleTree<Integer> tree = new SimpleTree<>(1);
+        tree.add(1, 2);
+        tree.add(1, 3);
+        tree.add(4, 5);
+        tree.add(5, 6);
+        assertThat(tree.isBinary()).isTrue();
+    }
+
+    @Test
+    void whenIsNotBinary() {
+        SimpleTree<Integer> tree = new SimpleTree<>(1);
+        tree.add(1, 2);
+        tree.add(1, 3);
+        tree.add(1, 4);
+        tree.add(4, 5);
+        tree.add(5, 6);
+        assertThat(tree.isBinary()).isFalse();
     }
 }
