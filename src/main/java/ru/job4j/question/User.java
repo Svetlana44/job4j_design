6141e5f107379ac2bdd2cfd6b178cf032a982606
SpS <shchaaas@mail.ru>
2023-01-03 22:06:53 +0300

    User model2.4

--- src/main/java/ru/job4j/question/User.java
+++ src/main/java/ru/job4j/question/User.java
@@ -1,4 +1,17 @@
 package ru.job4j.question;
 
+import java.util.Objects;
+
 public class User {
+
+    private int id;
+    private String name;
+
+    public User(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    /* Геттеры, сеттеры, equals() & hashCode() */
+
 }
