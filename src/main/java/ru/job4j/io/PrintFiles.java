a295ceafe26de8bb7cf53b845e62e16ce2876af5
SpS <shchaaas@mail.ru>
2023-02-16 22:07:56 +0300

    PrintFiles implements FileVisitor<Path>

---
+++ src/main/java/ru/job4j/io/PrintFiles.java
@@ -0,0 +1,36 @@
+package ru.job4j.io;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+
+import static java.nio.file.FileVisitResult.CONTINUE;
+
+public class PrintFiles implements FileVisitor<Path> {
+
+
+    @Override
+    public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+        return CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        System.out.println(file.toAbsolutePath());
+        return CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+        return CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+        return CONTINUE;
+    }
+    public static void main(String[] args) throws IOException {
+        Path start = Paths.get(".");
+        Files.walkFileTree(start, new PrintFiles());
+    }
+}
