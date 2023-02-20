079d6c0f4bcee478dacbc2119c8271bb18bd1570
SpS <shchaaas@mail.ru>
2023-02-20 21:32:01 +0300

    12. Валидация параметров запуска [#246865]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -2,6 +2,7 @@ package ru.job4j.io;
 
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.InvalidPathException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
@@ -9,8 +10,11 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get("./data");
-        search(start, p -> p.toFile().getName().endsWith(".txt")).forEach(System.out::println);
+
+        validation(args);
+
+        Path start = Paths.get(args[0]);
+        search(start, p -> p.toFile().getName().endsWith(args[1])).forEach(System.out::println);
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) {
@@ -21,5 +25,14 @@ public class Search {
             e.printStackTrace();
         }
         return searcher.getPaths();
+    }
+
+    public static boolean validation(String[] args) {
+        try {
+            java.nio.file.Paths.get(args[0] + args[1]);
+        } catch (InvalidPathException e) {
+            e.printStackTrace();
+        }
+        return true;
     }
 }
