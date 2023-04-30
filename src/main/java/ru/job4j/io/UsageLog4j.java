4bb7979e108e85fcbfcb6b9da920c9e92aefeca6
SpS <shchaaas@mail.ru>
2023-04-30 19:34:25 +0300

    2. Simple Loggin Facade 4 Java. [#268849]

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -7,12 +7,16 @@ log4j.appender.console.layout.ConversionPattern=%d{ISO8601} %5p %c:%M:%L - %m%n
 *  */
 package ru.job4j.io;
 
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
+/* import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;  */
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class UsageLog4j {
 
-    private static final Logger LOG = LogManager.getLogger(UsageLog4j.class.getName());
+ /* private static final Logger LOG = LogManager.getLogger(UsageLog4j.class.getName());   */
+    private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
         LOG.trace("trace message");
