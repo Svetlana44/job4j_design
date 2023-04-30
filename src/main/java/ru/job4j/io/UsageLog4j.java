a5670a70035efb70e04a26cc91d1a4ef80afe1d6
SpS <shchaaas@mail.ru>
2023-04-30 19:06:03 +0300

    1. Log4j. Логирование системы. [#95335]

---
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -0,0 +1,24 @@
+/* логирование
+* Создайте файл /src/main/resources/log4j.properties
+* log4j.rootLogger=DEBUG, console
+log4j.appender.console=org.apache.log4j.ConsoleAppender
+log4j.appender.console.layout=org.apache.log4j.PatternLayout
+log4j.appender.console.layout.ConversionPattern=%d{ISO8601} %5p %c:%M:%L - %m%n
+*  */
+package ru.job4j.io;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+public class UsageLog4j {
+
+    private static final Logger LOG = LogManager.getLogger(UsageLog4j.class.getName());
+
+    public static void main(String[] args) {
+        LOG.trace("trace message");
+        LOG.debug("debug message");
+        LOG.info("info message");
+        LOG.warn("warn message");
+        LOG.error("error message");
+    }
+}
