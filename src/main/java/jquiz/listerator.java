ef9a2433010a98f07944d8a9970b7d34c1a5be11
SPB <shchaaas@mail.ru>
2023-07-07 21:33:06 +0300

    jquiz

---
+++ src/main/java/jquiz/listerator.java
@@ -0,0 +1,23 @@
+package jquiz;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.stream.Stream;
+
+public class listerator {
+    static public void main(String[] args) {
+        List<String> list = new ArrayList<>();
+/*        ListIterator listIterator = list.listIterator(); */
+        list.add("1");
+        list.add("2");
+        list.add("3");
+        list.add("4");
+
+        Stream.iterate(
+                        list.listIterator(
+                                list.size()), i -> i.hasPrevious(), i -> i)
+                .forEach(i -> System.out.println(i.previous()));
+    }
+
+}
