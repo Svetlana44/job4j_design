1fd37d55868efae6571dd3da5dcdb9bf62a453a2
SpS <shchaaas@mail.ru>
2023-07-13 20:14:42 +0300

    1. Связи между таблицами [#373615  [#373615 #428561]]v3

--- src/main/java/mytest/ForDBF.java
+++ src/main/java/mytest/ForDBF.java
@@ -12,26 +12,26 @@ import java.io.IOException;
 
 public class ForDBF {
     public static void main(String[] args) throws IOException {
-        // Создаем новый файл dbf
+        /* Создаем новый файл dbf */
         File file = new File("example.dbf");
         Workbook workbook = new HSSFWorkbook();
         Sheet sheet = workbook.createSheet("Sheet1");
 
-        // Создаем заголовки столбцов
+        /* Создаем заголовки столбцов */
         Row headerRow = sheet.createRow(0);
         Cell headerCell1 = headerRow.createCell(0);
         headerCell1.setCellValue("Column1");
         Cell headerCell2 = headerRow.createCell(1);
         headerCell2.setCellValue("Column2");
 
-        // Создаем новую строку с данными
+        /* Создаем новую строку с данными */
         Row dataRow = sheet.createRow(1);
         Cell dataCell1 = dataRow.createCell(0);
         dataCell1.setCellValue("Value1");
         Cell dataCell2 = dataRow.createCell(1);
         dataCell2.setCellValue("Value2");
 
-        // Сохраняем файл
+        /* Сохраняем файл */
         FileOutputStream outputStream = new FileOutputStream(file);
         workbook.write(outputStream);
         workbook.close();
