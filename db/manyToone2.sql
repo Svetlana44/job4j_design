06e1177d2e8fb394d5e68255100b49da8fd16f18
SpS <shchaaas@mail.ru>
2023-07-15 19:07:19 +0300

    1. Связи между таблицами [#373615 # [#373615 #428561]] v2

--- db/manyToone2.sql
+++ db/manyToone2.sql
@@ -1,3 +1,8 @@
+create TABLE home(
+id serial primary key,
+place varchar(25)
+);
+
 create TABLE fixiks(
 id serial primary key,
 name varchar(15),
@@ -5,8 +10,3 @@ color varchar(10),
 age int(),
 home_id references home(id)
 );
-
-create TABLE home(
-id serial primary key,
-place varchar(25)
-)
