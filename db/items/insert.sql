6923040adcb8a8a9e73d1fb282262b6025808891
SpS <shchaaas@mail.ru>
2023-07-16 13:48:36 +0300

    2. Схема прав пользователи и роли. [#1093 # [#1093 #429791]] v6

--- db/items/insert.sql
+++ db/items/insert.sql
@@ -1,10 +1,14 @@
-insert into users(name,nikename,roles_id) values ('Andersen','Neo',1);
-
 insert into roles(name,users_id) values ('admin',1);
 
 insert into rules(name) values ('rwe');
 
-insert into rules(roles_id,rules_id) values (1,1);
+insert into roles_rules(roles_id,rules_id) values (1,1);
+
+insert into users(name,nikename,roles_id) values ('Andersen','Neo',1);
+
+insert into categories(name) values ('extra');
+
+insert into states(name) values ('done');
 
 insert into items(name,users_id,category_id,states_id) values ('itemName',1,1,1);
 
@@ -12,6 +16,4 @@ insert into comments(text,item_id) values ('comment for item',1);
 
 insert into attachs(files,item_id) values ('item.txt',1);
 
-insert into states(name) values ('done');
 
-insert into categories(name) values ('extra');
