32bf2a920c429d22885818d2ed4cc482f586f4f4
SpS <shchaaas@mail.ru>
2023-07-16 20:00:03 +0300

    1. Inner join [#373617  [#373617 #429800]]

--- db/innerjoin/innerjoin.sql
+++ db/innerjoin/innerjoin.sql
@@ -7,15 +7,18 @@ nikename varchar(15)
 create table roles(
 id serial primary key,
 name varchar(20),
-users_id int references user (id)
+users_id int references users (id)
 );
+
+insert into users(name,nikename) values('Jon','Konor');
+insert into roles(name,users_id) values('admin',1);
 
 select u.id,u.name as "User name", nikename, r.id, r.name RoleName from users u inner join roles r on u.id=r.users_id;
 
 select u.id,u.name as "User name", nikename, r.id, r.name
  from users u inner join roles r on u.id=r.users_id
- where "User name" like 'Jon';
+ where u.name like 'Jon';
 
 select u.id,u.name as "User name", nikename, r.id, r.name RoleName from users u inner join roles r on u.id=r.users_id
-where RoleName like 'admin';
+where r.name like 'admin';
 
