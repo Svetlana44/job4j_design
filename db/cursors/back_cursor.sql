4d18599c3ee7a4de7f1b960b0612e084a78a00d1
SpS <shchaaas@mail.ru>
2023-08-07 13:50:42 +0300

    5. Курсор в SQL [#504816  [#504816]]

---
+++ db/cursors/back_cursor.sql
@@ -0,0 +1,54 @@
+/* Чтобы подключиться к БД надо ввести команду
+psql --username=postgres
+*/
+
+drop table products;
+
+create table products (
+    id serial primary key,
+    name varchar(50),
+    count integer default 0,
+    price integer
+);
+
+insert into products (name, count, price) VALUES ('product_1', 1, 5);
+insert into products (name, count, price) VALUES ('product_2', 2, 10);
+insert into products (name, count, price) VALUES ('product_3', 3, 15);
+insert into products (name, count, price) VALUES ('product_4', 4, 20);
+insert into products (name, count, price) VALUES ('product_5', 5, 25);
+insert into products (name, count, price) VALUES ('product_6', 6, 30);
+insert into products (name, count, price) VALUES ('product_7', 7, 35);
+insert into products (name, count, price) VALUES ('product_8', 8, 40);
+insert into products (name, count, price) VALUES ('product_9', 9, 45);
+insert into products (name, count, price) VALUES ('product_10', 10, 50);
+insert into products (name, count, price) VALUES ('product_11', 11, 55);
+insert into products (name, count, price) VALUES ('product_12', 12, 60);
+insert into products (name, count, price) VALUES ('product_13', 13, 65);
+insert into products (name, count, price) VALUES ('product_14', 14, 70);
+insert into products (name, count, price) VALUES ('product_15', 15, 75);
+insert into products (name, count, price) VALUES ('product_16', 16, 80);
+insert into products (name, count, price) VALUES ('product_17', 17, 85);
+insert into products (name, count, price) VALUES ('product_18', 18, 90);
+insert into products (name, count, price) VALUES ('product_19', 19, 95);
+insert into products (name, count, price) VALUES ('product_20', 20, 100);
+
+/* - [[NO] SCROLL] – указание того, что курсор можно прокручивать или нет назад.
+Если указано NO SCROLL, то прокрутка назад невозможна.
+Если ничего не указано, то возможность прокрутки зависит от запроса. */
+BEGIN;
+DECLARE
+    cursor_products SCROLL cursor  for
+                        select * from products;
+
+FETCH BACKWARD
+        20 FROM cursor_products;
+
+MOVE BACKWARD 5 FROM cursor_products;
+MOVE BACKWARD 8 FROM cursor_products;
+MOVE BACKWARD 5 FROM cursor_products;
+
+FETCH NEXT FROM cursor_products;
+
+CLOSE cursor_products;
+
+COMMIT;
